86003772c401ae6e2d61917a38017e39550aadd0
Abiodun Israel Boluwatife <iaboluwatife@gmail.com>
2022-10-27 10:07:18 -0400

    Added few pics to be used later in this pages

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import Homepage from "./Pages/Homepage/Homepage"
 import Contactpage from "./Pages/Contactpage/Contactpage"
 import Aboutpage from "./Pages/Aboutpage/Aboutpage"
 import Footer from "./Components/Footer/Footer"
+import Businessdev from './Pages/Businessdevelopment/Businessdev';
 
 
 
 function App() {
   return (
     <div>
-      <Routes>
-      <Route path='/' element={<Homepage/>}/>
-      <Route path='Aboutpage' element={<Aboutpage/>} />
-      <Route path='Contactpage' element={<Contactpage/>} />
-    </Routes>
+      <Businessdev />
+      {/* <Routes>
+        <Route path='/' element={<Homepage/>}/>
+        <Route path='Aboutpage' element={<Aboutpage/>} />
+        <Route path='Contactpage' element={<Contactpage/>} />
+      </Routes> */}
       <Footer/>
       {/* <Homepage/>
       <Aboutpage/>
--- src/Pages/Businessdevelopment/Businessdev.jsx
+++ src/Pages/Businessdevelopment/Businessdev.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import bizdevStyle from "./Businessdev.module.css"
 
+
 const Businessdev = () => {
   return (
     <div>
         <section className={bizdevStyle.hero}>
-            <h1>Opportunities don't happen. Let’s create them for your business.</h1>
+            <h1 className={bizdevStyle.heading}>Opportunities don't happen. Let's create them for your business.</h1>
         </section>
     </div>
   )
