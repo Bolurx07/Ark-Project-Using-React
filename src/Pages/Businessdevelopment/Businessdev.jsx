de87d88ee2792db582e3613093f038dbf79dffd5
Abiodun Israel Boluwatife <iaboluwatife@gmail.com>
2022-10-27 09:43:42 -0400

    Created new files for Business Dev Page

---
+++ src/Pages/Businessdevelopment/Businessdev.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import bizdevStyle from "./Businessdev.module.css"
+
+const Businessdev = () => {
+  return (
+    <div>
+        <section className={bizdevStyle.hero}>
+            <h1>Opportunities don't happen. Let’s create them for your business.</h1>
+        </section>
+    </div>
+  )
+}
+
+export default Businessdev
