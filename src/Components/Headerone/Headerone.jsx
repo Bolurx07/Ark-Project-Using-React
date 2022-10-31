2f9d6e92d90fbb633d02cc13863bce860536d6ef
Abiodun Israel Boluwatife <iaboluwatife@gmail.com>
2022-10-30 22:13:34 -0400

    Added a props for logo to be passed into homepage

--- src/Components/Headerone/Headerone.jsx
+++ src/Components/Headerone/Headerone.jsx
@@ -3,10 +3,33 @@ import head1Style from "./Headerone.module.css"
 import { useState } from "react";
 import {Link} from 'react-router-dom'
 
-const Headerone = () => {
+const Headerone = ({logo}) => {
+
+     // Usestate for Hamburger display
+     const [toggle, setToggle] = useState(false)
+
+     const handleToggle = () => {
+       return setToggle(prevToggle =>!prevToggle)
+     }
+
   return (
     <div>
-        
+        <header className={head1Style.header}>
+        <div>
+          <img src={logo} alt="logo" />
+        </div>
+        <nav  className={toggle ? head1Style.active : head1Style.navbar }>
+            <ul>
+                <li id={head1Style.home}><Link to="/" >Home</Link></li>
+                <li><Link to="Aboutpage">About Us</Link></li>
+                <li><Link to="Contactpage">Contact Us</Link></li>
+                <li><button className={head1Style.navbutton}><Link to="#">Register</Link></button></li>
+            </ul>
+        </nav>
+        <div id={head1Style.hamburger} onClick={handleToggle}>
+            {toggle ? <div>&times;</div> :  <div>&#9776;</div>}
+        </div>
+      </header>
 
     </div>
   )
