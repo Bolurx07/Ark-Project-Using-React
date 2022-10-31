ccb997c6df33f3f6c91c694f8ec1910fb5de4270
Abiodun Israel Boluwatife <iaboluwatife@gmail.com>
2022-10-30 22:06:56 -0400

    Imported useState from react for hamburger toggle

---
+++ src/Components/Headerone/Headerone.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import head1Style from "./Headerone.module.css"
+import { useState } from "react";
+import {Link} from 'react-router-dom'
+
+const Headerone = () => {
+  return (
+    <div>
+        
+
+    </div>
+  )
+}
+
+export default Headerone
