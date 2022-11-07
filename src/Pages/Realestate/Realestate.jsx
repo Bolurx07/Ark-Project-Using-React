ee60a03a07729c05aaa347f67787cf0197e43d06
Abiodun Israel Boluwatife <iaboluwatife@gmail.com>
2022-11-07 10:18:29 -0500

    Added a background image to be used in this page

--- src/Pages/Realestate/Realestate.jsx
+++ src/Pages/Realestate/Realestate.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import realStyle from "./Realestate.module.css"
+import
 
 const Realestate = () => {
   return (
     <div>
-
+        <section className={realStyle.hero}>
+            <h1 className={realStyle.h1}>Building a network of positive solutions in the agricultural sector</h1>
+        </section>
     </div>
   )
 }
