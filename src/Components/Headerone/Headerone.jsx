26f983db6b87987246739e0c0f0c74dfdd19d26c
Israel <iaboluwatife@gmail.com>
2023-02-07 14:53:59 -0500

    Added react-icons dependency

--- src/Components/Headerone/Headerone.jsx
+++ src/Components/Headerone/Headerone.jsx
@@ -1,15 +1,38 @@
 import React from 'react'
 import head1Style from "./Headerone.module.css"
 import { useState } from "react";
-import {Link} from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { UserAuth } from '../../context/AuthContext';
+import { FaArrowCircleDown } from 'react-icons/fa';
+
 
 const Headerone = ({mainlogo, autoride}) => {
+
+  const { user, logOut } = UserAuth();
+  const navigate = useNavigate();
+  console.log(user)
+
+  const handleLogout = async () => {
+    try {
+      await logOut();
+      navigate('/');
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
      // Usestate for Hamburger display
      const [toggle, setToggle] = useState(false)
 
      const handleToggle = () => {
        return setToggle(prevToggle =>!prevToggle)
+     }
+
+     // Usestate for Logged-in User display
+     const [loginToggle, setLoginToggle] = useState(false)
+
+     const handleLoginToggle = () => {
+       return setLoginToggle(prevToggle =>!prevToggle)
      }
 
   return (
@@ -23,7 +46,26 @@ const Headerone = ({mainlogo, autoride}) => {
                   <li id={head1Style.home}><Link to="/" >Home</Link></li>
                   <li><Link to="Aboutpage">About Us</Link></li>
                   <li><Link to="/Contactpage">Contact Us</Link></li>
-                  <li><button className={head1Style.navbutton}><Link to="Signup">Register</Link></button></li>
+                  <li>
+                    {
+                      user?.email 
+                      ?
+                      <div>
+                        <div onClick={handleLoginToggle} className={head1Style.account}>
+                          <p>Account</p> 
+                          <FaArrowCircleDown />
+                        </div>
+                        <div className={loginToggle ? head1Style.showUser : head1Style.hideUser}>
+                          <p>Profile</p>
+                          <p>Settings</p>
+                          <p onClick={handleLogout}>Log Out</p>
+                        </div>
+                      </div>
+                      :
+                      <button className={head1Style.navbutton}><Link to="Signup">Register</Link></button>
+                    }
+                    
+                  </li>
               </ul>
           </nav>
           <div id={head1Style.hamburger} onClick={handleToggle}>
--- src/Components/Headertwo/Headertwo.jsx
+++ src/Components/Headertwo/Headertwo.jsx
@@ -5,6 +5,8 @@ import {Link} from 'react-router-dom'
 
 const Headertwo = ({logo, color, style}) => {
 
+    
+
     // Usestate for Hamburger display
     const [toggle, setToggle] = useState(false)
 
