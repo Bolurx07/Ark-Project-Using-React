853002f67fafc1814f7f52f8eade44c9fb1b15ab
Israel <iaboluwatife@gmail.com>
2023-02-16 21:26:30 -0500

    Added Validation Schema to the sign up page

--- src/Pages/Signup/Signup.jsx
+++ src/Pages/Signup/Signup.jsx
@@ -1,26 +1,72 @@
 import loginStyle from '../Login/Login.module.css';
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { useFormik } from 'formik';
 import { UserAuth } from '../../context/AuthContext';
 import logoblack from '../../Assets/logoblack.svg';
 import loginimg from '../../Assets/bizdevbackground.svg';
+import { signUpSchema } from '../../Schemas/Index';
+
+
+const onSubmit = async (values, actions) => {
+    // console.log(values);
+    // console.log(actions);
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    actions.resetForm()
+    
+}
 
 const Signup = () => {
+    
+    // Formik & Yup for State management
+    const { values, errors, touched, isValid, handleChange, handleBlur, handleSubmit } = useFormik({
+        initialValues: {
+            email: "",
+            password: ""
+        },
+        validationSchema: signUpSchema,
+        onSubmit,
+    });
+    
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    console.log(values)
+
+    // useState for UserAuth
+    // const [email, setEmail] = useState('');
+    // const [password, setPassword] = useState('');
     const { user, signUp } = UserAuth();
     const navigate = useNavigate()
+    
+    // const firebaseMailChange = (e) => {
+    //     setEmail(e.target.value)
+    // }
 
-    const handleFormSubmit = async (e) => {
+    // const firebasePasswordChange = (e) => {
+    //     setPassword(e.target.value)
+    // }
+
+    // const myHandleChange = () => {
+    //     handleChange;
+    //     firebaseMailChange();
+    //     firebasePasswordChange();
+    // }
+
+    // console.log(email)
+    const handleFirebaseSubmit = async (e) => {
         e.preventDefault();
         try {
-          await signUp(email, password);
+          await signUp(values.email, values.password);
             navigate('/Aboutpage')
         } catch (error) {
             console.log(error);
         }
     };
+
+    const handleFormSubmit = () => {
+        handleFirebaseSubmit();
+        handleSubmit;
+    }
+
 
     // console.log(signUp)
   return (
@@ -36,11 +82,30 @@ const Signup = () => {
         <section className={loginStyle.loginsect2}>
             <h2>SignUp!</h2>
             <p>Enter details to register.</p>
-            <form onSubmit={handleFormSubmit}>
-                <input onChange={(e) => setEmail(e.target.value)} type="email"  placeholder='Email' id={loginStyle.email} />
-                <input onChange={(e) => setPassword(e.target.value)} type="password"  placeholder='Password' id={loginStyle.password} />
+            <form onSubmit={handleFirebaseSubmit}>
+                <input 
+                    onChange={handleChange} 
+                    type="email"  
+                    placeholder='Email' 
+                    name='email'
+                    id={loginStyle.email} 
+                    value={values.email} 
+                    onBlur={handleBlur}
+                />
+                {errors.email && touched.email && <p className='error'>{errors.email}</p>}
+                <input 
+                    onChange={handleChange} 
+                    type="password"  
+                    name='password'
+                    placeholder='Password' 
+                    id={loginStyle.password}
+                    value={values.password} 
+                    onBlur={handleBlur}
+                />
+                {errors.password && touched.password && <p className='error'>{errors.password}</p>}
+
                 <span className={loginStyle.forgotpassword}>Forgot Password?</span>
-                <button> Sign Up </button>
+                <button disabled={!isValid}> Sign Up </button>
             </form>
             <div className={loginStyle.acctdiv}>
                 <p>Already have an account?</p>
--- src/Schemas/Index.js
+++ src/Schemas/Index.js
@@ -1,5 +1,8 @@
 import * as yup from "yup";
 
+const passwordRules = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
+
+// min 6 characters, 1 uppercase letter, 1 lowercase letter, 1 numeric digit
 
 
 export const signUpSchema = yup.object().shape({
@@ -7,7 +10,8 @@ export const signUpSchema = yup.object().shape({
     password: yup.string().min(6).matches(passwordRules, { message: "Please create a stronger password" })
 });
 
-export const logInSchema = yup.object().shape({
-    email: yup.string().email("Please enter a valid email").required("Required"),
-    password: yup.string().min(6).matches(passwordRules, { message: "Password is not strong" })
-});
+
+// export const logInSchema = yup.object().shape({
+//     email: yup.string().email("Please enter a valid email").required("Required"),
+//     password: yup.string().min(6).matches(passwordRules, { message: "Password is not strong" })
+// });
